8fae3eb8145c6df69787781e3fcafb9fcdcf7942
hashjaco <hashjaco@github.com>
2021-04-22 23:06:37 -0400

    modify indexRouter into default export(es6)

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const logger = require('morgan');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// routes
+const indexRouter = require('./routes/index');
+
+// attach utilities to application
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/v1', indexRouter);
+
+module.exports = app;
