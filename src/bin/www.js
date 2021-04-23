29a569c679c60bc4f90b96f3a216f4f26d0a6556
hashjaco <hashjaco@github.com>
2021-04-22 23:36:31 -0400

    refactor server at www.js to es6 syntax and implement error handler

--- src/bin/www.js
+++ src/bin/www.js
@@ -4,87 +4,74 @@
  * Module dependencies.
  */
 
-var app = require('../app');
-var debug = require('debug')('petstore-api:server');
-var http = require('http');
+import app from '../app'
+import debug from 'debug'
+import http from 'http'
 
 /**
  * Get port from environment and store in Express.
  */
-
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
+const normalizePort = val => {
+  const port = parseInt(val, 10)
+  if(Number.isNaN(port)){
     // named pipe
-    return val;
+    return val
   }
-
-  if (port >= 0) {
+  if (port >= 0){
     // port number
-    return port;
+    return port
   }
-
-  return false;
+  return false
 }
 
 /**
- * Event listener for HTTP server "error" event.
- */
+ * Get port from environment and store in Express
+ *  */
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
+/**
+ * Create HTTP server
+ * */
+const server = http.createServer(app)
+
+/**
+ * Event listener for HTTP server "error" event:
+ * This helper function will listen for errors in our http server and display meaningful messages
+ * */
+const onError = error => {
+  if(error.syscall !== 'listen'){
+    throw error
   }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
   // handle specific listen errors with friendly messages
-  switch (error.code) {
+  switch(error.code){
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
+      alert(`${bind} requires elevated privileges`)
+      process.exit(1)
+      break
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
+      alert(`${bind} is already in use`)
+      process.exit(1)
+      break
     default:
-      throw error;
+      throw error
   }
 }
 
 /**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+ * Event listener for HTTP server "listening" event:
+ * This function simply outputs the port the server is listening on to the console
+ * */
+const onListening = () => {
+  const addr = server.address()
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
+  debug(`Listening on ${bind}`)
 }
+
+/**
+ * Listen for incoming requests at the specified address over provided port, on all network interfaces
+ * */
+server.listen(port)
+server.on('error', onError)
+server.on('listening', onListening)
