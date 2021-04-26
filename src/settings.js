684644d54e1ca74dcee766c791b759b2495a6e7d
hashjaco <hashjaco@github.com>
2021-04-26 17:20:36 -0400

    setup database connection and queries

--- src/settings.js
+++ src/settings.js
@@ -2,3 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
+export const petfinderClientId = process.env.PETFINDER_API_KEY;
+export const petfinderClientSecret = process.env.PETFINDER_API_SECRET;
+export const connectionString = process.env.CONNECTION_STRING;
