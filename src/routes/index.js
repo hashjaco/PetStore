d45a96f01161f72a235b1f58c71038af0b2b5531
hashjaco <hashjaco@github.com>
2021-04-26 23:03:41 -0400

    messages entity post and get

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {getToken, indexPage, messagesPage } from "../controllers";
+import {getToken, indexPage, getMessages, addMessage} from "../controllers";
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/auth', getToken);
-indexRouter.get('/messages', messagesPage);
+indexRouter.get('/messages', getMessages);
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
--- test/messages.test.js
+++ test/messages.test.js
@@ -15,4 +15,22 @@ describe('Messages', () => {
         done();
       });
   });
+
+  it('should post messages', (done) => {
+    const data = { name: 'Cheryl Riley', message: 'Hashim is forever mine.' };
+    server
+      .post(`${BASE_URL}/messages`)
+      .send(data)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceof(Array);
+        res.body.messages.forEach((m) => {
+          expect(m).to.have.property('id');
+          expect(m).to.have.property('name', data.name);
+          expect(m).to.have.property('message', data.message);
+        });
+        done();
+      });
+  });
 });
