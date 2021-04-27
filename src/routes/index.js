180e12bea1b50023e8b8ed85b32dc26151acec59
hashjaco <hashjaco@github.com>
2021-04-26 23:23:40 -0400

    messages middleware function to modify outgoing message

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {getToken, indexPage, getMessages, addMessage} from "../controllers";
+import { modifyMessage } from "../middleware";
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/auth', getToken);
 indexRouter.get('/messages', getMessages);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
